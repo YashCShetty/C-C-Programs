cfd3737568e74e69c7181ea78085f512291d8bea
yash9356909 <yash9323@protonmail.com>
2023-01-22 00:00:57 +0530

    Circular Queue

---
+++ DynamicStack.c
@@ -0,0 +1,57 @@
+//
+// Created by yash on 20/1/23.
+//
+#include<stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+
+int stacksize=1;
+int top=-1;
+int *s;
+void push(int item){
+    if(top==stacksize-1){
+        printf("Stack Full Increased by 1\n");
+        stacksize++;
+        s=(int*)realloc(s,stacksize*sizeof(int));
+    }
+    s[++top]=item;
+}
+int pop()
+{
+    if(top==-1){
+        printf("Stack Underflow");
+        return -1;
+    }
+    printf("Stack size decreased by 1");
+    printf("Item deleted is %d",s[top--]);
+    stacksize--;
+    s=(int*)realloc(s,stacksize*sizeof(int));
+}
+void display(){
+    for(int i=top;i>=0;i--){
+        printf("%d \t",s[i]);
+    }
+}
+void main()
+{
+    int item,ch;
+    s=(int*)malloc(stacksize*sizeof(int));
+    while(1) {
+        printf("Welcome to stacks using dynamic array \n");
+        printf("1.Push \n2.Pop 3.Display 4.Exit \n ");
+        scanf("%d",&ch);
+        switch(ch)
+         {
+            case 1:
+            printf("Enter the item to be inserted \n");
+            scanf("%d", &item);
+            push(item);
+            break;
+            case 2:pop();
+            break;
+            case 3:display();
+            break;
+            case 4:exit(0);
+        }
+    }
+}
